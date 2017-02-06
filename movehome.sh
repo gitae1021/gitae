0155010d93904c02615777f933e89accd02ddfa2
arc-gt <gitae1021@github.com>
2017-02-06 17:26:32 +0900

    add script

---
+++ movehome.sh
@@ -0,0 +1,8 @@
+cp .bashrc ~
+cp -r .vim ~
+cp .vimrc ~
+cp .tmux.conf ~
+cp .ctags ~
+cp config ~/.ssh
+sudo cp -r colors/ /usr/share/vim/vim74
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
