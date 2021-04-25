dcce6b2f35e5d5fdae8c4edb3d7c95394902b171
gitae1021 <gitae1021>
2021-04-25 20:22:06 +0900

    delete redundancy

--- movehome.sh
+++ movehome.sh
@@ -1,12 +1,12 @@
 cp .bashrc ~
-cp -r .vim ~
+# cp -r .vim ~
 cp .vimrc ~
 cp .tmux.conf ~
 cp .ctags ~
-mkdir ~/.ssh
-cp config ~/.ssh
-cp install.sh ~
-sudo cp -r colors/ /usr/share/vim/vim74
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# mkdir ~/.ssh
+# cp config ~/.ssh
+# cp install.sh ~
+# sudo cp -r colors/ /usr/share/vim/vim74
+# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-vim -c "PluginInstall"
+# vim -c "PluginInstall"
